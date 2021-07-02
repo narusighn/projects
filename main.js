7e18b3a55a4aca4fa90237fe5c5d780fde256749
narusighn <80968987+narusighn@users.noreply.github.com>
2021-07-02 18:20:44 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-"he_double_hockeysticks"
+function login(){
+    var pl_1=document.getElementById("player_1").value;
+    var pl_2=document.getElementById("player_2").value;
+
+    localStorage.setItem("player1",pl_1);
+    localStorage.setItem("player2",pl_2);
+
+    window.location="math_quiz.html";
+}
