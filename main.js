796d06bb4befc92ea71f60e9583b054dcbf07f52
narusighn <80968987+narusighn@users.noreply.github.com>
2021-12-29 22:34:09 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,28 @@
-function log(){
-    var pl_1=document.getElementById("player_1").value;
-    var pl_2=document.getElementById("player_2").value;
+webcam.set({
+    width:350,
+    height:300,
+    img_format:'jpeg',
+    jpeg_quality:90
+});
 
-    localStorage.setItem("player1",pl_1);
-    localStorage.setItem("player2",pl_2);
+Webcam.attach("#camera");
 
-    window.location="math_quiz.html";
+function take_jpegIMG(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("finalProduct").innerHTML='<img id="finalIMG" src="'+data_uri+'">';
+         });
+    }
+
+console.log("ml5 version: ",ml5.version);
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/DPDspg3um/',modelLoaded);
+function modelLoaded(){
+    console.log("ml5 model is loaded");
+}
+
+function speak(){
+    var synth=window.speechSynthesis;
+    speak_data_1="The first Prediction is"+prediction_1;
+    speak_data_2="The second Prediction is"+prediction_2;
+    var utterthis=new SpeechSynthesisUtterance(speak_data_1=speak_data_2);
+    synth.speak(utterthis);
 }
