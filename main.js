0dcf8c8dfa781ba313d1c2bd68cec25370a7fa5c
narusighn <80968987+narusighn@users.noreply.github.com>
2021-12-31 22:30:55 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,28 +1,16 @@
-webcam.set({
-    width:350,
-    height:300,
-    img_format:'jpeg',
-    jpeg_quality:90
-});
+function preload(){
 
-Webcam.attach("#camera");
-
-function take_jpegIMG(){
-    Webcam.snap(function(data_uri){
-        document.getElementById("finalProduct").innerHTML='<img id="finalIMG" src="'+data_uri+'">';
-         });
-    }
-
-console.log("ml5 version: ",ml5.version);
-classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/DPDspg3um/',modelLoaded);
-function modelLoaded(){
-    console.log("ml5 model is loaded");
 }
 
-function speak(){
-    var synth=window.speechSynthesis;
-    speak_data_1="The first Prediction is"+prediction_1;
-    speak_data_2="The second Prediction is"+prediction_2;
-    var utterthis=new SpeechSynthesisUtterance(speak_data_1=speak_data_2);
-    synth.speak(utterthis);
+function draw(){
+
+}
+
+function setup(){
+    Owl=createCanvas(400,400);
+    Owl.position( 700,250);
+}
+
+function selfieTime(){
+    save('Cute-Doggy-You!.png');
 }
