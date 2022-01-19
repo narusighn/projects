dc54d3a058641830dde0c0d661ef07698d0f77f7
narusighn <80968987+narusighn@users.noreply.github.com>
2022-01-19 21:57:21 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,14 +3,32 @@ function preload(){
 }
 
 function draw(){
-
+image(video,0,0,400,400)
 }
 
 function setup(){
     Owl=createCanvas(400,400);
     Owl.position( 700,250);
+    video=createCapture(VIDEO);
+    video.size(400,400);
+    video.hide();
+
+    poseNet=ml5.poseNet();
+    poseNet.on('pose',gotPoses);
+}
+
+function modelLoaded(){
+    console.log("Model is intialized");
 }
 
 function selfieTime(){
     save('Cute-Doggy-You!.png');
+}
+
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+        console.log("nose_x = "+ results[0].pose.nose.x);
+        console.log("nose_y = "+ results[0].pose.nose.y);
+    }
 }
