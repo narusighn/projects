08e5d39e2cec1cc7959f28589f5d640d2ec1ce41
narusighn <80968987+narusighn@users.noreply.github.com>
2022-01-19 22:14:27 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
-function preload(){
+noseX=0;
+noseY=0;
 
+function preload(){
+    flower_nose=loadImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0scGGR7qvUfJ49xNuDtNiVFuyTrBE-5bitw&usqp=CAU");
 }
 
 function draw(){
-image(video,0,0,400,400)
+    image(video,0,0,400,400);
+    image(flower_nose,noseX,noseY,30,30);
 }
 
 function setup(){
@@ -27,8 +31,10 @@ function selfieTime(){
 
 function gotPoses(results){
     if(results.length>0){
+        noseX=results[0].pose.nose.x;
+        noseY=results[0].pose.nose.y;
         console.log(results);
-        console.log("nose_x = "+ results[0].pose.nose.x);
-        console.log("nose_y = "+ results[0].pose.nose.y);
+        console.log("nose_x = "+ results[0].pose.nose.x-5);
+        console.log("nose_y = "+ results[0].pose.nose.y-5);
     }
 }
