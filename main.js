9e2bba20f88cfc2003bdbf68172fc61cc5adca01
narusighn <80968987+narusighn@users.noreply.github.com>
2022-01-20 22:06:13 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,40 +1,17 @@
-noseX=0;
-noseY=0;
+TimerCunter=0;
+TimerCheck="";
+DrawnSketch="";
+Score=0;
 
-function preload(){
-    flower_nose=loadImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0scGGR7qvUfJ49xNuDtNiVFuyTrBE-5bitw&usqp=CAU");
-}
 
-function draw(){
-    image(video,0,0,400,400);
-    image(flower_nose,noseX,noseY,30,30);
-}
+quick_draw_data_set=["aircraft carrier","airplane","alarm clock","ambulance","angel","animal migration","ant","anvil","apple","arm","asparagus","axe","backpack","banana","bandage","barn","baseball","baseball bat","basket","basketball","bat","bathtub","beach","bear","beard","bed","bee","belt","bench","bicycle","binoculars","bird","birthday cake","blackberry","blueberry","book","boomerang","bottlecap","bowtie","bracelet","brain","bread","bridge","broccoli","broom","bucket","bulldozer","bus","bush","butterfly","cactus","cake","calculator","calendar","camel","camera","camouflage","campfire","candle","cannon","canoe","car","carrot","castle","cat","ceiling fan","cello","cell phone","chair","chandelier","church","circle","clarinet","clock","cloud","coffee cup","compass","computer","cookie","cooler","couch","cow","crab","crayon","crocodile","crown","cruise ship","cup","diamond","dishwasher","diving board","dog","dolphin","donut","door","dragon","dresser","drill","drums","duck","dumbbell","ear", "elbow","elephant","envelope","eraser","eye","eyeglasses","face","fan","feather","fence","finger","fire hydrant","fireplace","firetruck","fish","flamingo","flashlight","flip flops","floor lamp","flower","flying saucer","foot","fork","frog","frying pan","garden","garden hose","giraffe","goatee","golf club","grapes","grass","guitar","hamburger","hammer","hand","harp","hat","headphones","hedgehog","helicopter","helmet","hexagon","hockey puck","hockey stick","horse","hospital","hot air balloon","hot dog","hot tub","hourglass","house","house plant","hurricane","ice cream","jacket","jail","kangaroo","key","keyboard","knee","knife","ladder","lantern","laptop","leaf","leg","light bulb","lighter","lighthouse","lightning","line","lion","lipstick","lobster","lollipop","mailbox","map","marker","matches","megaphone","mermaid","microphone","microwave","monkey","moon","mosquito","motorbike","mountain","mouse","moustache","mouth","mug","mushroom","nail","necklace","nose","ocean","octagon","octopus","onion","oven","owl","paintbrush","paint can","palm tree","panda","pants","paper clip","parachute","parrot","passport","peanut","pear","peas","pencil","penguin","piano","pickup truck","picture frame","pig","pillow","pineapple","pizza","pliers","police car","pond","pool","popsicle","postcard","potato","power outlet","purse","rabbit","raccoon","radio","rain","rainbow","rake","remote control","rhinoceros","rifle","river","roller coaster","rollerskates","sailboat","sandwich","saw","saxophone","school bus","scissors","scorpion","screwdriver","sea turtle","see saw","shark","sheep","shoe","shorts","shovel","sink","skateboard","skull","skyscraper","sleeping bag","smiley face","snail","snake","snorkel","snowflake","snowman","soccer ball","sock","speedboat","spider","spoon","spreadsheet","square","squiggle","squirrel","stairs","star","steak","stereo","stethoscope","stitches","stop sign","stove","strawberry","streetlight","string bean","submarine","suitcase","sun","swan","sweater","swingset","sword","syringe","table","teapot","teddy-bear","telephone","television","tennis racquet","tent","The Eiffel Tower","The Great Wall of China","The Mona Lisa","tiger","toaster","toe","toilet","tooth","toothbrush","toothpaste","tornado","tractor","traffic light","train","tree","triangle","trombone","truck","trumpet","tshirt","umbrella","underwear","van","vase","violin","washing machine","watermelon","waterslide","whale","wheel","windmill","wine bottle","wine glass","wristwatch","yoga","zebra","zigzag"]
 
-function setup(){
-    Owl=createCanvas(400,400);
-    Owl.position( 700,250);
-    video=createCapture(VIDEO);
-    video.size(400,400);
-    video.hide();
+random_no = Math.floor((Math.random()*array_1.length)+1)
 
-    poseNet=ml5.poseNet(video,modelLoaded);
-    poseNet.on('pose',gotPoses);
-}
+console.log(quick_draw_data_set[random_no]);
 
-function modelLoaded(){
-    console.log("Model is intialized");
-}
+Sketch=quick_draw_data_set[random_no]
 
-function selfieTime(){
-    save('Cute-Doggy-You!.png');
-}
+document.getElementById("Sketchname").innerHTML="Sketch to be Drawn"+Sketch;
 
-function gotPoses(results){
-    if(results.length>0){
-        noseX=results[0].pose.nose.x-10;
-        noseY=results[0].pose.nose.y-10;
-        console.log(results);
-        console.log("nose_x = "+ results[0].pose.nose.x);
-        console.log("nose_y = "+ results[0].pose.nose.y);
-    }
-}
+
