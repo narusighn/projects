626c56c8297e609fe970a636715ff50ef2f13f0c
narusighn <80968987+narusighn@users.noreply.github.com>
2022-04-14 19:51:36 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
+var music1="";
+var music2="";
 
+
+function preload(){
+    music1=loadSound("music.mp3");
+    music2=loadSound("music2.mp3");
+}
+
+
+function setup(){
+    Platform=createCanvas("800,500");
+    Platform.center();
+
+    Mirror=createCapture(VIDEO);
+    Mirror.hide();
+}
+
+function draw(){
+    image(Mirror,0,0,800,500);
+}
