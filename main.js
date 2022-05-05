f311926b7197e2760ef8e07c89fae7b16473355a
narusighn <80968987+narusighn@users.noreply.github.com>
2022-05-05 19:50:26 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,61 +1,31 @@
-var music1="";
-var music2="";
-var l_wristX="";
-var l_wristY="";
-var r_wristX="";
-var r_wristY="";
-var score_LW="";
-var whichSong="";
-
-function preload(){
-    music1=loadSound("music.mp3");
-    music2=loadSound("music2.mp3");
-}
-
+picture="";
 
 function setup(){
-    Platform=createCanvas(800,500);
-    Platform.center();
+    canvas=createCanvas(900,700);
+    canvas.center();
 
-    Mirror=createCapture(VIDEO);
-    Mirror.hide();
-
-     Indentify=ml5.poseNet(Mirror,modelLoaded);
-    Indentify.on('pose',gotResults);
 }
+
+function change(){
+    window.location.href="index2.html"
+}
+
 
 function draw(){
-    image(Mirror,0,0,800,500);
-
-    fill("red");
-    stroke("black");
-    circle(l_wristX,l_wristY,20);
-
-    if(score_LW>=0.2){
-        music1.play();
-        document.getElementById("Song_name").innerHTML="Music1";
-        music2.setVolume(0);
+    if(but1=true){
+        picture="LivvingRoom.jpg";
+        image(picture,0,0,900,700);
     }
-
-}
-
-function start(){
-    music1.play();
-}
-
-function modelLoaded(){
-    console.log("model has intialised! You can do anything now, just do not hack!:)");
-}
-
-function gotResults(results){
-    if(results.lenght>0){
-        console.log(results);
-
-        l_wristX=results[0].pose.leftWrist.x;
-        l_wristY=results[0].pose.leftWrist.y;
-        r_wristX=results[0].ppose.rightWrist.x;
-        r_wristY=results[0].pose.rightWrist.y;
-
-        score_LW=results[0].pose.keypoints[9].score;
+    else if(but2=true){
+        picture="Kitchen.jpg";
+        image(picture,0,0,900,700);
+    }
+    else if(but3=true){
+        picture="pantry.jpg";
+        image(picture,0,0,900,700);
+    }    
+    else{
+        picture="dresser.jpg";
+        image(picture,0,0,900,700);
     }
 }
