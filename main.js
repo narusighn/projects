e69e4a2eaee2d1c8815f66cae1cae2ed26d8dea6
narusighn <80968987+narusighn@users.noreply.github.com>
2022-05-05 20:57:33 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,31 +1,44 @@
-picture="";
+img="";
+status="";
+things=[];
 
 function setup(){
-    canvas=createCanvas(900,700);
-    canvas.center();
-
+    camvas=createCanvas(800,500);
+    camvas.center();
+    objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+    console.log("narayani");
+    document.getElementById("status").innerHTML="status:object detection in progress"
 }
 
-function change(){
-    window.location.href="index2.html"
+function preload(){
+    img=loadImage("dog_cat.jpg");
 }
-
 
 function draw(){
-    if(but1=true){
-        picture="LivvingRoom.jpg";
-        image(picture,0,0,900,700);
+    image(img,0,0,800,500);
+    if(status==true){
+        for(i=0;i<things.length;i++){
+            document.getElementById("status").innerHTML="status:"+things.length+" object detected";
+            fill("black");
+            text(things[i].label,things[i].x,things[i].y);
+            stroke("black");
+            noFill();
+            rect(things[i].x,things[i].y,things[i].width,things[i].height);
+        }
     }
-    else if(but2=true){
-        picture="Kitchen.jpg";
-        image(picture,0,0,900,700);
+}
+
+function modelLoaded(){
+    console.log("Cocossd is loaded");
+    status=true;
+    objectDetector.detect(img,gotresults);
+}
+
+function gotresults(error,results){
+    if(error){
+        console.error(error);
     }
-    else if(but3=true){
-        picture="pantry.jpg";
-        image(picture,0,0,900,700);
-    }    
-    else{
-        picture="dresser.jpg";
-        image(picture,0,0,900,700);
-    }
+    
+    console.log(results);
+    things=results;
 }
