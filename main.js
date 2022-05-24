a3dbafe983f2ac621a26549e8f99cc4c4557542f
narusighn <80968987+narusighn@users.noreply.github.com>
2022-05-24 18:35:42 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,8 +3,11 @@ status="";
 things=[];
 
 function setup(){
-    camvas=createCanvas(800,500);
+    camvas=createCanvas(380,380);
     camvas.center();
+    video=createCapture(VIDEO);
+    video.size(380,380);
+    video.hide()
     objectDetector=ml5.objectDetector('cocossd',modelLoaded);
     console.log("narayani");
     document.getElementById("status").innerHTML="status:object detection in progress"
@@ -15,13 +18,19 @@ function preload(){
 }
 
 function draw(){
-    image(img,0,0,800,500);
-    if(status==true){
+    image(video,0,0,380,380);
+    if(status!=""){
+        r=random(255);
+        g=random(255);
+        b=random(255);
+        objectDetector.detect(video,gotresults);
         for(i=0;i<things.length;i++){
-            document.getElementById("status").innerHTML="status:"+things.length+" object detected";
-            fill("black");
-            text(things[i].label,things[i].x,things[i].y);
-            stroke("black");
+            document.getElementById("status").innerHTML="status: object detected";
+            document.getElementById("Nobjects").innerHTML="Number of object detected are: "+things.length;
+            fill(r,g,b);
+            confidence=Math.floor(things[i].confidence*100);
+            text(things[i].label+" "+confidence+"%",things[i].x,things[i].y);
+            stroke(r,g,b);
             noFill();
             rect(things[i].x,things[i].y,things[i].width,things[i].height);
         }
@@ -31,7 +40,6 @@ function draw(){
 function modelLoaded(){
     console.log("Cocossd is loaded");
     status=true;
-    objectDetector.detect(img,gotresults);
 }
 
 function gotresults(error,results){
