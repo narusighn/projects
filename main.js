f0c28709d28ad76b8283700b05feb95f3d38d843
narusighn <80968987+narusighn@users.noreply.github.com>
2022-07-19 21:38:00 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,20 +1,18 @@
 function setup(){
-    canvas=createCanvas(700,500);
-    canvas.center();
-    video=createCapture(VIDEO);
-    video.hide()
+    cnv=createCanvas(600,300);
+    cnv.parent("Canvas");
+    cam=createCapture(VIDEO);
+    cam.size(600,300);
+    cam.hide();
+
+    pose=ml5.poseNet(cam,modelLoaded);
+}
+
+function modelLoaded(){
+    console.log("Elon Musks says AI robots are dangerous!!!!! Please listen o the Tesla guy");
 }
 
 function draw(){
-    img(video,300,200,700,500);
-    fill("red");
-    circle(20,40,40);
-    circle(20,660,40);
-    circle(20,40,460);
-    circle(20,660,450);
-    fill("green");
-    rect(40,460,20,500);
-    rect(40,450,20,500);
-    rect(40,660,20,700);
-    rect(40,660,20,700);
+    img(cam,0,0,600,300);
+
 }
